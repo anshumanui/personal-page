b11a701553bdb04ebc14c3cffd64f14a273c51c5
nataliavokrouhlecka <natalia.vokrouhlecka@gmail.com>
2019-11-19 00:13:03 +0100

    Add basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,32 @@ import Footer from './Footer';
 
 function App() {
   return (
-    <div className="homepage">
+    <React.Fragment>
       <Header 
         title="Natalia's space to share and experiment"
         link="Once to be a link to all articles"
       />
-      <Article 
-        title="My favourite podcasts"
-        content="My favourite podcasts Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc auctor. Aliquam erat volutpat. Nullam faucibus mi quis velit. Vestibulum fermentum tortor id mi. Duis pulvinar. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Aliquam ante. Vivamus porttitor turpis ac leo. Maecenas aliquet accumsan leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas." 
+      <div className="homepage">
+        <Article 
+          title="My favourite podcasts"
+          content="My favourite podcasts Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc auctor. Aliquam erat volutpat. Nullam faucibus mi quis velit. Vestibulum fermentum tortor id mi. Duis pulvinar. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Aliquam ante. Vivamus porttitor turpis ac leo. Maecenas aliquet accumsan leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas." 
+          />
+        <Article 
+          title="Article on Kids Academy to come"
+          content="Kids Academy Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc auctor. Aliquam erat volutpat. Nullam faucibus mi quis velit. Vestibulum fermentum tortor id mi. Duis pulvinar. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Aliquam ante. Vivamus porttitor turpis ac leo. Maecenas aliquet accumsan leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+          " 
+          />
+        <Article 
+          title="Article on Code Retreat to come"
+          content="Kids Academy Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc auctor. Aliquam erat volutpat. Nullam faucibus mi quis velit. Vestibulum fermentum tortor id mi. Duis pulvinar. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Aliquam ante. Vivamus porttitor turpis ac leo. Maecenas aliquet accumsan leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+          " 
+          />
+        <Footer 
+          text="I am a footer"
+          link="Once to become a link to the top of the page"
         />
-      <Article 
-        title="Article on Kids Academy to come"
-        content="Kids Academy Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc auctor. Aliquam erat volutpat. Nullam faucibus mi quis velit. Vestibulum fermentum tortor id mi. Duis pulvinar. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Aliquam ante. Vivamus porttitor turpis ac leo. Maecenas aliquet accumsan leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-        " 
-        />
-      <Article 
-        title="Article on Code Retreat to come"
-        content="Kids Academy Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc auctor. Aliquam erat volutpat. Nullam faucibus mi quis velit. Vestibulum fermentum tortor id mi. Duis pulvinar. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Aliquam ante. Vivamus porttitor turpis ac leo. Maecenas aliquet accumsan leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-        " 
-        />
-      <Footer 
-        text="I am a footer"
-        link="Once to become a link to the top of the page"
-      />
-    </div>
+      </div>
+    </React.Fragment>
   );
 }
 
