fb527598ad3ec6c9de5333079d22c2fd6cd59a32
nataliavokrouhlecka <natalia.vokrouhlecka@gmail.com>
2019-12-30 20:02:45 +0100

    Add about me

---
+++ src/AboutMe.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Card, Icon, Avatar } from 'antd';
+const { Meta } = Card;
+
+class AboutMe extends React.Component {
+  render() {
+    return <Card
+      style={{ width: 900 }}
+    >
+      <Meta
+        avatar={<Avatar src="" />}
+        title="About me"
+        description="Hi, I'm Natalia! I'm a web developer. I like to organise events and make things happen."
+      />
+    </Card>
+  }
+}
+
+export default AboutMe;
