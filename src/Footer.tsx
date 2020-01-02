45d2755699e1baf9ebc75da4481f731df83851a0
nataliavokrouhlecka <natalia.vokrouhlecka@gmail.com>
2020-01-02 20:17:01 +0100

    Add icon to footer and change text

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Footer.css'
 
 export type Props = {
   text: string;
@@ -11,6 +12,7 @@ const MyFooter: React.SFC<Props> = ({
   <footer className="footer">
     <p>{text}</p>
     <a href="#">{link}</a>
+    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/nataliavokrouhlecka/"><i className="fab fa-linkedin-in footer-icon"></i></a>
   </footer>
 )
 
