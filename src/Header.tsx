57bbda8b916a6e176071d7bad50ccd0734e56175
nataliavokrouhlecka <natalia.vokrouhlecka@gmail.com>
2020-01-04 17:08:08 +0100

    #13 Remove unused link

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import AboutMe from './AboutMe';
 
 export type Props = {
-  title: string;
-  link?: string;
+  title: string
 }
 
 const MyHeader: React.SFC<Props> = ({
-  link, title
+  title
 }) => (
   <div className="header">
     <h1>{title}</h1>
-    <a href="#">{link}</a>
     <AboutMe />
   </div>
 );
